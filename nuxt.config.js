a1e0110df076c4d39ab50893533a21f9291c0f70
Cody Bontecou <bontecouc@gmail.com>
2019-08-26 09:38:57 -0600

    Title and lang configured in head {}

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,9 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'Wowrares | Classic Rare Mob Spawns',
+    lang: 'en',
+    title: 'Wowrares',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
