8a79620b02f6ed9c4d3f77e819191ff6a10ec750
Cody Bontecou <bontecouc@gmail.com>
2019-08-26 09:41:54 -0600

    trying lowercase on links

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,7 +20,7 @@
               :to="{
                 name: 'zone-id',
                 params: {
-                  id: zone.name
+                  id: zone.name.toLowerCase()
                 }
               }"
             >
